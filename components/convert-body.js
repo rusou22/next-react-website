4ecf3f0de38fc753570742fd3370836332fe5501
rusou22 <KTC23A31C0003@edu.kyoto-tech.ac.jp>
2024-01-26 16:25:10 +0900

    commit

--- components/convert-body.js
+++ components/convert-body.js
@@ -21,3 +21,4 @@ export default function ConvertBody({ contentHTML }) {
     })
 return <>{contentReact}</>
 }
+
---
+++ pages/blog/[slug].js
@@ -0,0 +1,108 @@
+import { getPostBySlug, getAllSlugs } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import { prevNextPost } from 'lib/prev-next-post'
+import Meta from 'components/meta'
+import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column' 
+import ConvertBody from 'components/convert-body' 
+import PostCategories from 'components/post-categories'
+import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
+import PostBody from '@/components/post-body'
+
+//ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+export default function Post({
+    title,
+    publish,
+    content,
+    eyecatch,
+    categories,
+    description,
+    prevPost,
+    nextPost,
+}) {
+    return (
+        <Container>
+            <Meta
+                pageTitle={title}
+                pageDesc={description}
+                pageImg={eyecatch.url}
+                pageImgW={eyecatch.width}
+                pageImgH={eyecatch.height}
+            />
+
+            <article>
+
+                <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+                <figure>
+                    <Image
+                        src={eyecatch.url}
+                        alt=""
+                        layout="responsive"
+                        width={eyecatch.width}
+                        height={eyecatch.height}
+                        sizes="(min-width:1152px) 1152px,100vw"
+                        priority
+                        placeholder = 'blur'
+                        blurDataURL = {eyecatch.blurDataURL}
+                    />
+                </figure>
+
+                <TwoColumn>
+                    <TwoColumnMain>
+                        <PostBody><ConvertBody contentHTML={content} /></PostBody>
+                    </TwoColumnMain>
+                    <TwoColumnSidebar>
+                        <PostCategories categories={categories} />
+                    </TwoColumnSidebar>
+                </TwoColumn>
+                <div>{prevPost.title} {prevPost.slug}</div>
+                <div>{nextPost.title} {nextPost.slug}</div>
+            </article>
+        </Container>
+    )
+}
+
+export async function getStaticPaths() {
+    
+    const allSlugs = await getAllSlugs()
+
+    return {
+        paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+        fallback:false,
+    }
+}
+
+export async function getStaticProps(context) {
+    const slug = context.params.slug
+
+    const post = await getPostBySlug(slug)
+
+    const description = extractText(post.content)
+
+    const eyecatch = post.eyecatch ?? eyecatchLocal
+
+    const { base64 } = await getPlaiceholder(eyecatch.url)
+    eyecatch.blurDataURL = base64
+
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs,slug)
+
+
+    return {
+        props: {
+            title: post.title,
+            publish: post.content,
+            content: post.content,
+            eyecatch: eyecatch,
+            categories: post.categories,
+            description: description,
+            prevPost: prevPost,
+            nextPost: nextPost,
+
+        },
+    }
+}
